812713ad0da6965401c17306d660d4177c079293
unknown <ederphil@gmail.com>
2021-01-05 00:29:50 -0300

    GET /products

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dsdeliver.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsdeliver.dto.ProductDto;
+import com.devsuperior.dsdeliver.entities.Product;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Transactional(readOnly = true)
+	public List<ProductDto> findAll() {
+
+		List<Product> listProduct = productRepository.findAllByOrderByNameAsc();
+
+		return listProduct.stream().map(x -> new ProductDto(x)).collect(Collectors.toList());
+
+	}
+
+}
