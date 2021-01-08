af627ae81b41128535f0abd5f47b98f4e7f3c2e9
unknown <ederphil@gmail.com>
2021-01-08 10:10:06 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
-import Hello from './Hello';
-import Counter from './counter';
+
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar/>
-    </div>
+      <Home/>
+    </>
   );
 }
 
