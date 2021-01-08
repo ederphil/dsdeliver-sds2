5dfb5b728398568c0a88771f21761aff2a59e0ba
unknown <ederphil@gmail.com>
2021-01-08 03:54:46 -0300

    Create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Hello from './Hello';
+import Counter from './counter';
+
+function App() {
+  return (
+    <div>
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
