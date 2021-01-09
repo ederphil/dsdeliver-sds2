6911756a5c415003b64fd91638a9614acc41d0c0
unknown <ederphil@gmail.com>
2021-01-09 02:24:30 -0300

    Final aula 2.1

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import ProductList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './style.css';
+import { Product } from './Types';
 
 function Orders() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products);
+    
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+
+    }, []);
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductList products={products} />
         </div>
     )
 }
